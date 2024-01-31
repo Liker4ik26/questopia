4bbb4fef6fd4e7c78b655fa3b0716e0f3782baae
liker4ik26 <likakan85@gmail.com>
2024-01-31 20:36:52 +0600

    [Feat] Added dimensions

---
+++ lib/src/app/styles/dimensions.dart
@@ -0,0 +1,9 @@
+class AppDimensions {
+  AppDimensions._();
+
+  static const double extraSmall = 6;
+  static const double small = 8;
+  static const double tinyMedium = 16;
+  static const double medium = 24;
+  static const double circle = 100;
+}
